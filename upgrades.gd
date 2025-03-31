66a158b9215b3e74b9e691dcfcc8c2cb3caf8970
La Popi 3 <63921014+Alexandru191919@users.noreply.github.com>
2025-03-31 18:53:27 +0200

    isajdnfda

--- upgrades.gd
+++ upgrades.gd
@@ -12,12 +12,10 @@ func _on_timer_UFO_timeout():
 	
 	$"../TextureScene2".hide()
 	$"Upgrade1-Energy".hide()
-	$"Upgrade2-MoneyCap".hide()
 	$"Upgrade3-MoneyGain".hide()
 	$"Upgrade4-CustomerTemper".hide()
 	
 	$"Upgrade1-Energy/ProgressBars-Upgrade1".hide()
-	$"Upgrade2-MoneyCap/ProgressBars-Upgrade2".hide()
 	$"Upgrade3-MoneyGain/ProgressBars-Upgrade3".hide()
 	$"Upgrade4-CustomerTemper/ProgressBars-Upgrade4".hide()
 	
